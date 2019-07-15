c6da38d3924129f3ffc9240e1e1d583c2f2a4788
erniker <josepablomedinagrande@gmail.com>
2019-07-15 20:30:03 +0200

    changes in helloworld class

--- product/controllers/helloworld.js
+++ product/controllers/helloworld.js
@@ -1,5 +1,8 @@
-function helloworld()
-{
-     res.status(200).json({ code: 'Hellow helloworld!' })
-     return 
+class helloworld {
+  helloworld() {
+    res.status(200).json({ code: 'Hellow helloworld!' })
+    return
+  }
 }
+
+module.exports = new helloworld()
