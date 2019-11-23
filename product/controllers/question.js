c229561a9a2b2946ebc15805890462c290f8e5fa
Francisco Zurita <zurit92@gmail.com>
2019-11-23 12:09:06 +0100

    next question controller updated

--- product/controllers/question.js
+++ product/controllers/question.js
@@ -1,10 +1,10 @@
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 class Question {
   list(req, res) {
     req.models.question
       .findAll({
-        order: [
-          ['id', 'DESC']
-        ],
+        order: [['id', 'DESC']],
         include: [
           {
             model: req.models.answer
@@ -13,17 +13,24 @@ class Question {
       })
       .then(questions => {
         res.json(questions)
-        })
-        .catch(err =>
-        {
-          console.log(err)
-        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
+
   nextQuestion(req, res) {
     req.query.include = [{ model: req.models.answer }]
+    req.query.where = { id: { [Op.notIn]: req.body.questionViewed } }
+
     req.models.question.findAll(req.query).then(questions => {
+      console.log(questions)
       let index = Math.floor(Math.random() * questions.length)
-      res.json({ question: questions[index], settings: req.body })
+
+      res.json({
+        question: questions[index],
+        settings: req.body
+      })
     })
   }
 }
